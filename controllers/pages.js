70f7152eedca318d2dc18a4bfc31bd209a5fbd80
Fedor <mydearfrodo@yandex.ru>
2016-03-26 16:06:12 +0500

    init4

--- controllers/pages.js
+++ controllers/pages.js
@@ -1,6 +1,15 @@
-
+var todos = [
+    'Сходить на ДММ',
+    'Купить котика',
+    'Навестить бабулю',
+    'Сделать 5ую задачу'
+];
 exports.index = (req, res) => {
     res.render('index.html');
 };
+exports.listTodo = (req, res) => {
+    res.send(todos);
+};
+
 
 exports.error404 = (req, res) => res.sendStatus(404);
