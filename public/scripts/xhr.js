198424ff4bc37145caaa6e4a330d6aed2737a01e
Michael Uskov <uskovm@gmail.com>
2016-03-30 23:41:14 +0500

    Added XHR lib

---
+++ public/scripts/xhr.js
@@ -0,0 +1,35 @@
+'use strict';
+
+(function () {
+	window.XHR = {};
+	
+	var __makeQuery = function (method, url, body, cb) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url, true);
+		xhr.onreadystatechange = function () {
+			if (xhr.readyState != 4) {
+				return;
+			}
+			cb(xhr.status, JSON.parse(xhr.responseText));
+		};
+		body ? xhr.send(body) : xhr.send();
+		return xhr;
+	};
+	
+	window.XHR.getJSON = function (url, cb) {
+		__makeQuery('GET', url, undefined, cb);
+	};
+	
+	window.XHR.postJSON = function (url, bodyObject, cb) {
+		__makeQuery('POST', url, JSON.stringify(bodyObject), cb);
+	};
+	
+	window.XHR.putJSON = function (url, bodyObject, cb) {
+		__makeQuery('PUT', url, JSON.stringify(bodyObject), cb);
+	};
+	
+	window.XHR.deleteJSON = function (url, bodyObject, cb) {
+		__makeQuery('DELETE', url, undefined, cb);
+	};
+	
+})();
