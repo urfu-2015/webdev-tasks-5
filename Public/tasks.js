8f54babe9a5d05bed4de9e7112f21d5ec05352ca
Natalia Aksenova <dotokoto@gmail.com>
2016-03-31 01:34:42 +0500

    nope, now it shows tasks

--- Public/tasks.js
+++ Public/tasks.js
@@ -1,9 +1,11 @@
 'use strict';
 
 function xhrRequest(method, url, async, data, callback) {
+    console.log('in xhrRequest');
     const xhr = new XMLHttpRequest();
     xhr.open(method, url, async);
     xhr.onreadystatechange = function () {
+        console.log('readyState: ' + xhr.readyState);
         if (xhr.readyState != 4) {
             return;
         }
@@ -11,8 +13,9 @@ function xhrRequest(method, url, async, data, callback) {
             console.log(xhr.status + ': ' + xhr.statusText);
             return;
         }
+        console.log('call callback');
         if (callback) {
-            callback(xhr.responseText);
+            callback(null, xhr.responseText);
         }
     };
     if (data) {
@@ -21,23 +24,30 @@ function xhrRequest(method, url, async, data, callback) {
     } else {
         xhr.send();
     }
+    console.log('sent');
 }
 
 function getTasks() {
+    console.log('in gettasks');
     xhrRequest('GET', '/tasks', true, {}, function (error, data) {
+        console.log('make xhrrequest in gettasks');
         if (error) {
             console.log(error);
             return;
         }
-        if (data.status === 200) {
-            showAllTasks(data.content);
-        }
+        console.log('status:');
+        console.log(data);
+        data = JSON.parse(data);
+        console.log(data.content);
+        showAllTasks(data.content);
     });
 }
 
 function showAllTasks(storage) {
     // Есть контейнер, надо из него удалить все которые были и добавить новые
-    const container = document.getElementsByClassName('task-container');
+    console.log('in showAlltasks');
+    console.log(storage);
+    const container = document.getElementsByClassName('task-container')[0];
     removeChildren(container);
     storage.forEach(function (element) {
         container.appendChild(createTaskItem(element));
@@ -52,9 +62,11 @@ function createTaskItem(task) {
     span.innerHTML = task.text;
     div.appendChild(span);
     const textArea = document.createElement('textarea');
+    textArea.className = 'task-item__textarea task-item__textarea_hidden';
     div.appendChild(textArea);
     const button = document.createElement('button');
-    button.className = 'task-item__button-send';
+    button.className = 'task-item__button task-item__button_hidden';
+    button.innerHTML = 'Сохранить';
     div.appendChild(button);
     const divDelete = document.createElement('div');
     divDelete.className = 'task-item__delete task-item__delete_hidden';
@@ -63,10 +75,40 @@ function createTaskItem(task) {
 }
 
 function removeChildren(node) {
-    var children = node.childNodes;
+    const children = node.childNodes;
     while (children.length) {
         node.removeChild(children[0]);
     }
 }
 
+function addTask () {
+    const task = {text: 'Task'};
+    xhrRequest('POST', '/tasks', true, task, function (error, data) {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        if(data.status === 200) {
+            const taskDiv = createTaskItem(task);
+            var container = document.getElementsByClassName('task-container')[0];
+            container.appendChild(taskDiv);
+        }
+    });
+}
+
+
+function addTaskEvent (event) {
+    tap(event, addTask);
+}
+
+function tap (event) {
+
+}
+
+
+//--------------------------------------------------------
 getTasks();
+const buttonAdd = document.getElementsByClassName('task-controllers__button-add')[0];
+console.log('buttonAdd');
+console.log(buttonAdd);
+//buttonAdd.addEventListener('touchstart', addTaskEvent);
