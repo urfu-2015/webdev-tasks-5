f49969b12d6a39c6b701293df58ece0c0c841c79
v-samoylov <samoylov.vladimir@yahoo.com>
2016-04-01 20:18:43 +0500

    + saving tasks on the server

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,16 @@
 const tasks = require('../tasks/tasks.js')
 
 exports.getAll = (req, res) => {
-    res.status(200).send(JSON.stringify({tasks}));
+    res.status(200).json({tasks});
 };
 
 exports.post = (req, res) => {
+    var task = req.body;
+    tasks.forEach((_task) => {
+        if (_task.id === task.id) {
+            _task.text = task.text;
+            res.status(201).json({tasks});
+        }
+    });
 };
 
