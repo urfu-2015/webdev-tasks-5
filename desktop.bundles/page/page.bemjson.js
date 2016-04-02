5fdf1ec1dcf8689545d2dc8aff4465675accd7ea
danmir <danmir@me.com>
2016-04-02 17:35:08 +0500

    Browser babel type

--- desktop.bundles/page/page.bemjson.js
+++ desktop.bundles/page/page.bemjson.js
@@ -6,7 +6,7 @@
         { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1.0, user-scalable=yes' } }
     ],
     scripts: [
-        { elem: 'js', url: '_page.js' },
+        { elem: 'js', attrs: { type: 'text/babel'}, url: '_page.js' },
         { elem: 'js', url: 'fetch.js' },
         { elem: 'js', url: 'es6-promise.min.js' },
         { elem: 'js', url: 'promise.min.js' },
