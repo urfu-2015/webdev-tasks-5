d5da4937704ea4c7721c3d010e3bca743bb3b687
Латыпова Екатерина < >
2016-04-04 12:17:12 +0500

    исправления react в main

--- client/components/notes.js
+++ client/components/notes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Note from './note';
 
-export default ({notes, store}) => (
-    <ul className="notes">
+export default ({notes, store}) => {
+    return (
+        <ul className="notes">
         {notes.map(note => (
             <Note key={note.task} task={note.task} id={note.id} store={store} />
         ))}
     </ul>
-);
+    );
+};
