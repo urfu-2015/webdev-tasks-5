c93f2a3a3df6a635451a05d4e65eba30c37c94ab
Екатерина Онуфриенко < >
2016-04-05 17:30:37 +0500

    Правки без реакта

--- client/components/header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default () => (
-    <header>ToDo-хи</header>
-);
--- views/index.js
+++ views/index.js
@@ -1,13 +1,12 @@
 refresh();
 
-document.addEventListener('touchstart', setTouchStartEvent, false);
-document.addEventListener('touchend', setTouchEndEvent, false);
-
 var addButton = document.querySelector('.butn-to-add');
 addButton.addEventListener('click', function (event) {
     var newText = document.querySelector('.text-to-add').value;
     newText ? addTask(newText) : alert('Нельзя добавить пустое задание');
 });
+
+ontouch(document, pullAndRefreshEvent, false);
 
 function refresh() {
     var xhr = new XMLHttpRequest();
@@ -15,24 +14,18 @@ function refresh() {
     xhr.send();
     xhr.onreadystatechange = function () {
         if (xhr.status === 200 && xhr.readyState === 4) {
+            var loader = document.querySelector('.pull-and-refresh__loader');
+            loader.style.display = 'none';
+
             var refreshBlock = document.querySelector('.pull-and-refresh');
-            refreshBlock.style.display = 'none';
+            refreshBlock.style.height = '0px';
+            refreshBlock.classList.add('pull-and-refresh-hidden');
+
             var taskContainer = document.querySelector('.tasks-container');
             taskContainer.innerHTML = xhr.responseText;
-        }
-    }
-}
-
-function addTask(text) {
-    document.querySelector('.text-to-add').value = '';
-    var xhr = new XMLHttpRequest();
-    var params = 'text=' + encodeURIComponent(text);
-    xhr.open('POST', '/', true);
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(params);
-    xhr.onreadystatechange = function () {
-        if (xhr.status === 200 && xhr.readyState === 4) {
-            refresh();
+            [].forEach.call(document.querySelectorAll('.task'), function (task) {
+                swipeHandler(task);
+            });
         }
     }
 }
@@ -63,91 +56,146 @@ function editTask(id, text) {
     }
 }
 
-var startX,
-    startY,
-    startT;
-
-function setTouchStartEvent(event) {
-    startX = event.changedTouches[0].pageX;
-    startY = event.changedTouches[0].pageY;
-    startT = new Date().getTime();
-    var element = event.target;
-
-    if (event.targetTouches.length === 1 &&
-        element.id.indexOf('delete') != -1) {
-        var id = element.id.split('_')[1];
-        removeTask(id);
+function addTask(text) {
+    document.querySelector('.text-to-add').value = '';
+    var xhr = new XMLHttpRequest();
+    var params = 'text=' + encodeURIComponent(text);
+    xhr.open('POST', '/', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    xhr.onreadystatechange = function () {
+        if (xhr.status === 200 && xhr.readyState === 4) {
+            refresh();
+        }
     }
 }
 
-function setTouchEndEvent(event) {
-    var deltaX = event.changedTouches[0].pageX - startX,
-        deltaY = event.changedTouches[0].pageY - startY,
-        deltaT = new Date().getTime() - startT,
-        element = event.target,
-        minUpDelta = 70,
-        delta = 15;
-
-    var isSwipeToUp = deltaY >= minUpDelta;
-    var isHorizontalSwipe = Math.abs(deltaY) < delta;
-    var isSwipeToLeft = deltaX <= -delta;
-    var isSwipeToRight = deltaX > 0;
-    var isTheSame = deltaX === 0 && deltaY === 0 && deltaT > delta;
-
-    if (isSwipeToUp) {
-        pullAndRefreshEvent();
-    }
-    if (isHorizontalSwipe && isSwipeToLeft) {
-        swipeToLeft(element);
-    }
-    if (isHorizontalSwipe && isSwipeToRight) {
-        swipeToRight(element);
-    }
-    if (isTheSame) {
-        touchToEdit(element);
-    }
-}
-
-function pullAndRefreshEvent() {
+function pullAndRefreshEvent(event, direction, phase, swipetype, distance) {
     var parElement = document.querySelector('.pull-and-refresh');
-    parElement.style.display = 'block';
-    setTimeout(refresh, 2000);
-}
+    var oldHeight =  parseFloat(window.getComputedStyle(parElement).height);
+    var maxRefreshBlockHeight = 100;
+    var loader = document.querySelector('.pull-and-refresh__loader');
 
-function swipeToLeft(element) {
-    if (element.className.indexOf('task') != -1) {
-        var id = element.id.split('_')[1];
-        if (!hasDeleteDiv(id)) {
-            var task = document.querySelector('#task_' + id);
-            var deleteDiv = document.createElement('div');
-            deleteDiv.className = 'task__delete';
-            deleteDiv.id = 'delete_' + id;
-            deleteDiv.innerHTML = '<i class="fa fa-trash"></i>';
-            task.appendChild(deleteDiv);
-        }        
+    if (direction === 'down') {
+        var newHeight = Math.min(oldHeight + distance, maxRefreshBlockHeight);
+        parElement.style.height = newHeight + 'px';
+        parElement.classList.remove('pull-and-refresh-hidden');
+        
+        if (newHeight === maxRefreshBlockHeight) {
+            loader.style.display = 'block';
+            setTimeout(refresh, 1500);
+        }
+    }
+
+    if (direction === 'up') {
+        loader.style.display = 'none';
+        parElement.style.height = '0px';
+        parElement.classList.add('pull-and-refresh-hidden');
     }
 }
 
-function hasDeleteDiv(id) {
-    var deleteDiv = document.querySelector('#delete_' + id);
-    var res = deleteDiv ? true: false;
-    return res;
+function swipeHandler (node) {
+    var maxDeleteButtonWidth = parseFloat(window.getComputedStyle(node).width) * 0.25;
+    var deleteButton = node.querySelector('.task__delete');
+
+    ontouch(node, function (event, direction, phase, swipetype, distance) {
+        var element = event.target; 
+        var isTap = event.targetTouches.length === 1;
+        var isTheSame = direction === 'none' && !distance;
+        //tap to delete
+        if (phase === 'start') {
+            var toDelete = element.id.indexOf('delete') != -1;
+            if (isTap && toDelete) {
+                setDeleteEvent(element);
+            }
+        }
+        //tap to edit
+        if (phase === 'end' && isTheSame) {
+            var toEdit = element.id.indexOf('task') != -1 ||
+                element.id.indexOf('text') != -1;
+            if (toEdit) {
+                touchToEdit(element);
+            }
+        }
+        var buttonWidth = parseFloat(window.getComputedStyle(deleteButton).width);
+        //swipe to left
+        if (direction === 'left') {
+            deleteButton.classList.remove('task__delete-hidden');
+            var newWidth = Math.min(buttonWidth + Math.abs(distance), maxDeleteButtonWidth);
+            deleteButton.style.width = newWidth + 'px';
+        }
+        //swipe to right
+        if (direction === 'right') {
+            var newWidth = Math.max(0, buttonWidth - distance);
+            if (newWidth === 0) {
+                deleteButton.classList.add('task__delete-hidden');
+            }
+            deleteButton.style.width = newWidth + 'px';
+        }
+    });
+};
+
+function ontouch (element, callback) {
+    var dir,
+    swipeType,
+    startX,
+    startY,
+    distX,
+    distY,
+    threshold = 150,
+    restraint = 100,
+    allowedTime = 500,
+    deltaTime,
+    startTime;
+ 
+    element.addEventListener('touchstart', function (event) {
+        dir = 'none';
+        swipeType = 'none';
+        dist = 0;
+        startX = event.changedTouches[0].pageX;
+        startY = event.changedTouches[0].pageY;
+        startTime = new Date().getTime();
+        callback(event, 'none', 'start', swipeType, 0); 
+    }, false)
+ 
+    element.addEventListener('touchmove', function (event) {
+        var touchobj = event.changedTouches[0]
+        distX = touchobj.pageX - startX;
+        distY = touchobj.pageY - startY;
+        
+        if (Math.abs(distX) > Math.abs(distY)) {
+            dir = (distX < 0)? 'left' : 'right'
+            callback(event, dir, 'move', swipeType, distX);
+        } else {
+            dir = (distY < 0)? 'up' : 'down';
+            callback(event, dir, 'move', swipeType, distY);
+        }
+    }, false)
+ 
+    element.addEventListener('touchend', function (event) {
+        var touchobj = event.changedTouches[0]
+        deltaTime = new Date().getTime() - startTime;
+
+        if (deltaTime <= allowedTime) {
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) {
+                swipeType = dir // "left" or "right"
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) {
+                swipeType = dir // "up" or "down"
+            }
+        }
+        callback(event, dir, 'end', swipeType, (dir =='left' || dir =='right')? distX : distY);
+    }, false)
 }
 
-function swipeToRight(element) {
-    if (element.className.indexOf('task') != -1) {
-        var id = element.id.split('_')[1];
-        var task = document.querySelector('#task_' + id);
-        var node = document.querySelector('#delete_' + id);
-        task.removeChild(node);
-        task.className = 'task';
-    }
+function setDeleteEvent (element) {
+    var id = element.id.split('_')[1];
+    removeTask(id);
 }
 
 function touchToEdit(element) {
     var id = element.id.split('_')[1];
-    var isCorrectObj = element.id.indexOf('task') != -1 || element.id.indexOf('text') != -1;
-    if (isCorrectObj && !checkFormExists()) {
+    if (!checkFormExists()) {
         var task = document.querySelector('#task_' + id);        
         var oldText = document.querySelector('#text_' + id).innerHTML;
         task.innerHTML = '';
@@ -177,7 +225,7 @@ function createEditForm(id, oldText) {
     inputSubmit.type = 'submit';
     inputSubmit.setAttribute('value', 'Сохранить');
     inputSubmit.className = 'task__edit-submit'; 
-    inputSubmit.id = 'edit-submit_' + id;    
+    inputSubmit.id = 'edit-submit_' + id;
     form.appendChild(inputSubmit);
 
     return form;
