280589f782588a26291b37f36addee70fce9c0d5
hx0day <hx0day@hackerdom.ru>
2016-04-07 02:46:33 +0500

    feat(*): add react v0.4

--- client/bundles/todo/todo.js
+++ client/bundles/todo/todo.js
@@ -5,6 +5,9 @@ import Footer from '../footer/footer.js';
 var initialPoint;
 var finalPoint;
 
+var initialUpdatePoint;
+var finalUpdatePoint;
+
 const TodoItem = ({text, todo_id}) => (
     <section id={todo_id} className="todo-list__item" onTouchStart={onTouchStart}
              onTouchEnd={viewTrash}>
@@ -18,23 +21,22 @@ const TodoItem = ({text, todo_id}) => (
 );
 function onTouchStart(event) {
     event.preventDefault();
-    event.stopPropagation();
     initialPoint = event.changedTouches[0];
 }
 function viewTrash(event) {
     var element = event.currentTarget;
     event.preventDefault();
-    //event.stopPropagation();
     finalPoint = event.changedTouches[0];
     var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
     var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
     if (xAbs === 0 && yAbs === 0) {
-
-        var todo_input = element.querySelector('.todo-list__item_input');
-        var todo_text = element.querySelector('.todo-list__item_text');
-        todo_text.style = 'display:none';
-        todo_input.style = 'display:block';
-        todo_input.focus();
+        if (event.target.tagName.toLowerCase() != "img") {
+            var todo_input = element.querySelector('.todo-list__item_input');
+            var todo_text = element.querySelector('.todo-list__item_text');
+            todo_text.style = 'display:none';
+            todo_input.style = 'display:block';
+            todo_input.focus();
+        }
     } else if (xAbs > 20 || yAbs > 20) {
         var todo_trash = element.querySelector('.todo-list__item_trash');
         if (xAbs > yAbs) {
@@ -102,9 +104,30 @@ const TodoForm = (todos) => (
     </form>
 );
 
+function updateTouchStart() {
+    event.preventDefault();
+    initialUpdatePoint = event.changedTouches[0];
+}
+
+function updateTouchEnd() {
+    event.preventDefault();
+    //event.stopPropagation();
+    finalUpdatePoint = event.changedTouches[0];
+    var xAbs = Math.abs(initialUpdatePoint.pageX - finalUpdatePoint.pageX);
+    var yAbs = Math.abs(initialUpdatePoint.pageY - finalUpdatePoint.pageY);
+    var element = event.currentTarget;
+    console.log(element);
+    if (xAbs > 20 || yAbs > 20) {
+        if (xAbs < yAbs) {
+            if (finalPoint.pageY > initialPoint.pageY) {
+                update('d');
+            }
+        }
+    }
+}
 
 export default ({todos}) => (
-    <div>
+    <div onTouchStart={updateTouchStart} onTouchEnd={updateTouchEnd}>
         <div className="todo-list">
             {todos.map(todo => (
 
